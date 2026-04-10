f12e474fe376ea78a9ff1efd84b5bdbb21b3a691
sreejabiswas065-svg <sreejabiswas065@gmail.com>
2026-04-10 16:06:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import json
 
 from sklearn.model_selection import train_test_split
